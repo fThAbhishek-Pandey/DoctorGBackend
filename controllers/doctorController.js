394add4e5670263f4a92c7691c62877d58afe839
fThAbhishek-Pandey <abhishek830564@email.com>
2024-10-08 14:20:22 +0530

    change viligibility

---
+++ controllers/doctorController.js
@@ -0,0 +1,16 @@
+import DoctorModel from "../modles/DoctorModle.js"
+
+const changeAvailablity = async (req,res)=>{
+        try {
+            const {docId} = req.body;
+            console.log(docId)
+            const docData = await DoctorModel.findById(docId);
+            console.log(docData);
+            await DoctorModel.findByIdAndUpdate(docId,{available: !docData.available}) 
+            res.json ({success:true, message: 'Availablity Changed'})
+        } catch (error) {
+            console.log("changeAvailability : ",error);
+            res.json({success:false,message:error.message});
+        }
+}
+export default changeAvailablity;
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -6,11 +6,13 @@ console.log("addDoctor",addDoctor);
 import upload from '../middleware/multer.js'
 import multer from '../middleware/multer.js';
 import AllDoctors from '../controllers/AllDoctors.js';
+import changeAvailablity from '../controllers/doctorController.js';
 console.log("multer03: ",multer)
 const adminRouter = express.Router();
 // console.log('I am admin routes',addDoctor)
 adminRouter.post('/add-doctor',authAdmin,upload.single('image'),addDoctor);
 adminRouter.post('/login',loginAdmin);
 adminRouter.get('/all-doctors',authAdmin,AllDoctors);
+adminRouter.get('/change-visibility',changeAvailablity);
 // console.log("admin router02",adminRouter)
 export default adminRouter;
